cae635190b4add4477507686ab543814ae481e64
damirsolutioncode <damir.solutioncode@gmail.com>
2022-10-16 18:57:46 +0500

    some random comments on a broken code

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
-    public void printGreatest(int first, int second) {
+    public static void main(String[] args){
+
+        int first=0;
+        int second=0;
+        {
+            if (first > second) {
+                System.out.println(first);
+            } else {
+                System.out.println(second);
+            }
+        }
     }
 }
